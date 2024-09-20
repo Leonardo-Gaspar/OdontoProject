5943acb1af1e2c229928f575b5d0237513b286e8
@Leonardo-Gaspar-Saheb <lgaspar@sdnadigital.com>
2024-09-20 15:09:55 -0300

    Update

--- agente_okto_2/vector_okto.py
+++ agente_okto_2/vector_okto.py
@@ -15,7 +15,6 @@ def load_txt_files(file_paths):
             documents.append(Document(page_content=content, metadata={"source": file_path}))
     return documents
 
-# Utilizando um modelo de embeddings mais leve e rápido: sentence-transformers/all-MiniLM-L6-v2
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 text_splitter = RecursiveCharacterTextSplitter(
@@ -24,42 +23,38 @@ text_splitter = RecursiveCharacterTextSplitter(
     separators=['\n\n', '\n', '.', ' ', '']
 )
 
-# Diretório para salvar o índice FAISS
 faiss_index_path = "faiss_index"
 
 txt_files = {
-    "afiliados_TeC": "afiliados_TeC.txt",
-    "apostas_de_quota_fixa": "apostas_de_quota_fixa.txt",
-    "autorizacoes_aposta": "autorizacoes_aposta.txt",
-    "loterias": "loterias.txt",
-    "politica_de_bonus": "politica_de_bonus.txt",
-    "politica_de_jogo_responsavel": "politica_de_jogo_responsavel.txt",
-    "politica_de_privacidade": "politica_de_privacidade.txt",
-    "politica_de_reclamacao": "politica_de_reclamacao.txt",
-    "promocao_comercial": "promocao_comercial.txt",
-    "promocoes_comerciais": "promocoes_comerciais.txt",
-    "regra_de_apostas": "regra_de_apostas.txt",
-    "seguranca": "seguranca.txt",
-    "termos_e_condicao": "termos_e_condicao.txt",
+    "Lei": ["okto_documents/apostas_de_quota_fixa.txt",
+            "okto_documents/autorizacoes_aposta.txt",
+            "okto_documents/loterias.txt",
+            "okto_documents/promocao_comercial.txt",
+            "okto_documents/promocoes_comerciais.txt"
+            ],
+    "Documentacao": [ "okto_documents/afiliados_TeC.txt",
+                      "okto_documents/politica_de_bonus.txt",
+                      "okto_documents/politica_de_jogo_responsavel.txt",
+                      "okto_documents/politica_de_privacidade.txt",
+                      "okto_documents/politica_de_reclamacao.txt",
+                      "okto_documents/regra_de_apostas.txt",
+                      "okto_documents/seguranca.txt",
+                      "okto_documents/termos_e_condicao.txt"
+                      ]
 }
 
-# Processando cada arquivo TXT e criando as coleções separadas
 for collection_name, txt_file in txt_files.items():
     print(f"Processando {collection_name}...")
 
-    # Carregar os arquivos TXT e extrair o texto
     documents = load_txt_files([txt_file])
 
-    # Dividir o conteúdo dos textos em partes menores
     split_docs = text_splitter.split_documents(documents)
 
-    # Criar ou conectar ao índice FAISS para essa coleção específica
     vectordb = FAISS.from_documents(
         documents=split_docs,
         embedding=embedding_model
     )
 
-    # Salvar o índice FAISS para uso posterior
     with open(f"{faiss_index_path}_{collection_name}.pkl", "wb") as f:
         pickle.dump(vectordb, f)
 
